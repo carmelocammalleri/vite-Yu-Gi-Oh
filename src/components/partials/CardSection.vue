7348d504fc3b65629352eff6e722652d250da580
Melozzo <carmecamma1995@gmail.com>
2023-10-12 17:50:57 +0200

    [link]: style scss of card section

--- src/components/partials/CardSection.vue
+++ src/components/partials/CardSection.vue
@@ -1,9 +1,22 @@
 <script>
+
   export default{
     name: 'CardSection',
   }
 </script>
 
 <template>
-
+  <div class="d-flex flex-wrap">
+    <!-- carte -->
+    <div class="card yugiCard text-center ">
+      <img src="/public/img/Cavaliere-del-Re-EEN-Rara-Carte-Yu-Gi-Oh.jpg" alt="cavaliere nero">
+      <h4>cavaliere nero</h4>
+      <p>alien</p>
+    </div>
+    
+  </div>
 </template>
+
+<style lang="scss" scoped>
+  @use '../../scss/cardSection.scss' as *;
+</style>
