db56825e21cf4e5bd0ca2f62fcce18ed4fa9a722
Melozzo <carmecamma1995@gmail.com>
2023-10-12 18:17:58 +0200

    [import]: store in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <script>
   import Header from './components/Header.vue'
   import Main from './components/Main.vue'
+  import { store } from './data/store';
 
   export default {
     name: 'App',
     components: {
       Header,
       Main
+    },
+    Data(){
+      return{
+        store
+      }
     }
   }
 </script>
