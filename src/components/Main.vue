47be315ada9b3b888dab5c9ce4b3d0527805b280
Melozzo <carmecamma1995@gmail.com>
2023-10-13 14:59:33 +0200

    [add]: v-for in  select's options

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -22,10 +22,10 @@
 
   <main class="mainContainer container"> 
     <select class="archetype my-3" name="Archetype" id="">
-      <option value="alien"> alien</option>
-      <option value="aliens"> aliens</option>
-      <option value="aliend"> aliend</option>
-      <option value="alienf"> alienf</option>
+      <option 
+        v-for="card in store.cardsList" 
+        :key="card.id" 
+        :value="card.archetype"> {{card.archetype}} </option>
     </select>
     <div class="sectionCard">
       <span class="finder d-block">ho trovato 5 carte</span>
