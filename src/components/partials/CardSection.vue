5c451211e1f9b814cdb7cf2db12e736d45113d1f
Melozzo <carmecamma1995@gmail.com>
2023-10-13 14:50:02 +0200

    [remove]: div in card section and add props

--- src/components/partials/CardSection.vue
+++ src/components/partials/CardSection.vue
@@ -1,40 +1,25 @@
 <script>
 
   export default{
-    name: 'CardSection',
+    name: "cardSection",
+
+    props: {
+    nameObj: String,
+    typeObj: String,
+    imageObj: String
+  }
   }
 </script>
 
 <template>
-  <div class="d-flex flex-wrap justify-content-between  ">
     <!-- carte -->
-    <div class="card yugiCard text-center ">
-      <img src="/public/img/Cavaliere-del-Re-EEN-Rara-Carte-Yu-Gi-Oh.jpg" alt="cavaliere nero">
-      <h6>cavaliere nero</h6>
-      <p>alien</p>
+    <div class="card yugiCard text-center">
+      <img :src="imageObj" 
+      alt="nameObj">
+      <h6>{{nameObj}}</h6>
+      <p>{{typeObj}}</p>
     </div>
-    <div class="card yugiCard text-center ">
-      <img src="/public/img/Cavaliere-del-Re-EEN-Rara-Carte-Yu-Gi-Oh.jpg" alt="cavaliere nero">
-      <h6>cavaliere nero</h6>
-      <p>alien</p>
-    </div>
-    <div class="card yugiCard text-center ">
-      <img src="/public/img/Cavaliere-del-Re-EEN-Rara-Carte-Yu-Gi-Oh.jpg" alt="cavaliere nero">
-      <h6>cavaliere nero</h6>
-      <p>alien</p>
-    </div>
-    <div class="card yugiCard text-center ">
-      <img src="/public/img/Cavaliere-del-Re-EEN-Rara-Carte-Yu-Gi-Oh.jpg" alt="cavaliere nero">
-      <h6>cavaliere nero</h6>
-      <p>alien</p>
-    </div>
-    <div class="card yugiCard text-center ">
-      <img src="/public/img/Cavaliere-del-Re-EEN-Rara-Carte-Yu-Gi-Oh.jpg" alt="cavaliere nero">
-      <h6>cavaliere nero</h6>
-      <p>alien</p>
-    </div>
-    
-  </div>
+
 </template>
 
 <style lang="scss" scoped>
